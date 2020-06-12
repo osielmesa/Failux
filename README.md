af1b07debf6ea82a5ca40aed60b06666e210f52f
Osiel <osiel.lima@marvintech.net>
2020-06-12 08:31:49 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Failux
+
+Failux is a simplified and broken implementation of Redux.
+In the sandbox you'll find a small application written in Failux
+and React.
+
+This is not working: stopwatch where you can change the update
+interval.
+
+When you click on "start", the stopwatch should start and after
+a specified update interval increases its value by the value of
+the interval.
+
+When you click on "stop", the stopwatch must stop and reset its
+value. 
+
+Fork sandbox and fix issues as well as potentially
+problematic code. Make code modern and more readable. 
+Comment your solution.
+
+https://codesandbox.io/s/angry-cdn-iphrw
---
+++ src/index.js
@@ -0,0 +1,150 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const createStore = (reducer, initialState) => {
+  let currentState = initialState;
+  const listeners = [];
+
+  const getState = () => currentState;
+  const dispatch = action => {
+    currentState = reducer(currentState, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = listener => listeners.push(listener);
+
+  return { getState, dispatch, subscribe };
+};
+
+const connect = (mapStateToProps, mapDispatchToProps) => Component => {
+  class WrappedComponent extends React.Component {
+    render() {
+      return (
+        <Component
+          {...this.props}
+          {...mapStateToProps(this.context.store.getState(), this.props)}
+          {...mapDispatchToProps(this.context.store.dispatch, this.props)}
+        />
+      );
+    }
+
+    componentDidUpdate() {
+      this.context.store.subscribe(this.handleChange);
+    }
+
+    handleChange = () => {
+      this.forceUpdate();
+    };
+  }
+
+  WrappedComponent.contextTypes = {
+    store: PropTypes.object
+  };
+
+  return WrappedComponent;
+};
+
+class Provider extends React.Component {
+  getChildContext() {
+    return {
+      store: this.props.store
+    };
+  }
+
+  render() {
+    return React.Children.only(this.props.children);
+  }
+}
+
+Provider.childContextTypes = {
+  store: PropTypes.object
+};
+
+// APP
+
+// actions
+const CHANGE_INTERVAL = "CHANGE_INTERVAL";
+
+// action creators
+const changeInterval = value => ({
+  type: CHANGE_INTERVAL,
+  payload: value
+});
+
+// reducers
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_INTERVAL:
+      return (state += action.payload);
+    default:
+      return {};
+  }
+};
+
+// components
+
+class IntervalComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>Update interval: {this.props.currentInterval} sec</span>
+        <span>
+          <button onClick={() => this.props.changeInterval(-1)}>-</button>
+          <button onClick={() => this.props.changeInterval(1)}>+</button>
+        </span>
+      </div>
+    );
+  }
+}
+
+const Interval = connect(
+  dispatch => ({
+    changeInterval: value => dispatch(changeInterval(value))
+  }),
+  state => ({
+    currentInterval: state
+  })
+)(IntervalComponent);
+
+class TimerComponent extends React.Component {
+  state = {
+    currentTime: 0
+  };
+
+  render() {
+    return (
+      <div>
+        <Interval />
+        <div>Timer: {this.state.currentTime} sec</div>
+        <div>
+          <button onClick={this.handleStart}>Start</button>
+          <button onClick={this.handleStop}>Stop</button>
+        </div>
+      </div>
+    );
+  }
+
+  handleStart() {
+    setTimeout(() =>
+        this.setState({
+          currentTime: this.state.currentTime + this.props.currentInterval
+        }),
+      this.props.currentInterval
+    );
+  }
+
+  handleStop() {
+    this.setState({ currentTime: 0 });
+  }
+}
+
+const Timer = connect(state => ({currentInterval: state}), () => {})(TimerComponent);
+
+// init
+ReactDOM.render(
+  <Provider store={createStore(reducer)}>
+    <Timer />
+  </Provider>,
+  document.getElementById("app")
+);
